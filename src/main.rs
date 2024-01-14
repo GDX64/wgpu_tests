284464b46868e0711642779cca1d728d8fe0b677
gdx <gadu.macahdo@gmail.com>
2024-01-14 13:37:58 -0300

    simulation kind of works

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-const WIDTH: usize = 640;
-const HEIGHT: usize = 360;
+const PIXEL_WIDTH: usize = 300;
+const PIXEL_HEIGHT: usize = 300;
+const PARTICLE_NUMBER: usize = 100;
+const SCALING: f64 = 5.;
+const WIDTH: f64 = PIXEL_WIDTH as f64 / SCALING;
+const HEIGHT: f64 = PIXEL_HEIGHT as f64 / SCALING;
+
 use minifb::{Window, WindowOptions};
 use particle::{World, V2};
 use piet::{
-    kurbo::{Circle, Rect},
+    kurbo::{Affine, Circle, Line, Rect},
     Color, ImageBuf,
 };
 use piet_common::Device;
@@ -17,8 +22,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn draw_app() -> Result<(), Box<dyn Error>> {
     let mut window = Window::new(
         "Test - ESC to exit",
-        WIDTH,
-        HEIGHT,
+        PIXEL_WIDTH,
+        PIXEL_HEIGHT,
         WindowOptions::default(),
     )
     .unwrap_or_else(|e| {
@@ -26,21 +31,23 @@ fn draw_app() -> Result<(), Box<dyn Error>> {
     });
 
     let rng = || rand::random::<f64>();
-    let mut world = World::new(V2::new(WIDTH as f64, HEIGHT as f64), V2::new(0., 9.));
-    world.add_random_particles(5, rng);
+    let mut world = World::new(V2::new(WIDTH, HEIGHT), V2::new(0., 9.));
+    world.add_random_particles(PARTICLE_NUMBER, rng);
 
     // Limit to max ~60 fps update rate
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
     let mut device = Box::new(Device::new()?);
     while window.is_open() {
-        let mut target = device.bitmap_target(WIDTH, HEIGHT, 1.)?;
+        let mut target = device.bitmap_target(PIXEL_WIDTH, PIXEL_HEIGHT, 1.)?;
         {
             let mut piet_context = target.render_context();
             world.evolve();
             draw(&mut piet_context, &world);
         };
         let drawing = buff_to_vec(target.to_image_buf(piet::ImageFormat::RgbaPremul)?);
-        window.update_with_buffer(&drawing, WIDTH, HEIGHT).unwrap();
+        window
+            .update_with_buffer(&drawing, PIXEL_WIDTH, PIXEL_HEIGHT)
+            .unwrap();
     }
     Ok(())
 }
@@ -63,11 +70,23 @@ fn buff_to_vec(buff: ImageBuf) -> Vec<u32> {
 
 fn draw(piet_context: &mut impl piet::RenderContext, world: &World) {
     let brush = piet_context.solid_brush(Color::WHITE);
+    piet_context.transform(Affine::scale(SCALING));
     world.particles.iter().for_each(|particle| {
         let x = particle.position.x;
         let y = particle.position.y;
-        let rect = Circle::new((x, y), 10.);
+        let rect = Circle::new((x, y), 1.);
         piet_context.fill(rect, &brush);
     });
+    // let center = V2::new(WIDTH as f64 / 2., HEIGHT as f64 / 2.);
+    // let gradient = world.calc_gradient(&center);
+    // draw_arrow(&center, &center.add(&gradient), piet_context);
     piet_context.finish().unwrap();
+}
+
+fn draw_arrow(p1: &V2, p2: &V2, piet_context: &mut impl piet::RenderContext) {
+    let brush = piet_context.solid_brush(Color::WHITE);
+    let line = Line::new((p1.x, p1.y), (p2.x, p2.y));
+    piet_context.stroke(line, &brush, 1.0);
+    let rect = Rect::new(p2.x - 5., p2.y - 5., p2.x + 5., p2.y + 5.);
+    piet_context.fill(rect, &brush);
 }
--- src/particle.rs
+++ src/particle.rs
@@ -1,3 +1,6 @@
+use std::f64::consts::PI;
+
+#[derive(Clone, Debug, PartialEq)]
 pub struct V2 {
     pub x: f64,
     pub y: f64,
@@ -7,8 +10,25 @@ impl V2 {
     pub fn new(x: f64, y: f64) -> V2 {
         V2 { x, y }
     }
+
+    pub fn sub(&self, other: &V2) -> V2 {
+        V2::new(self.x - other.x, self.y - other.y)
+    }
+
+    pub fn add(&self, other: &V2) -> V2 {
+        V2::new(self.x + other.x, self.y + other.y)
+    }
+
+    pub fn len(&self) -> f64 {
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+
+    pub fn scalar_mul(&self, scalar: f64) -> V2 {
+        V2::new(self.x * scalar, self.y * scalar)
+    }
 }
 
+#[derive(Clone, Debug)]
 pub struct Particle {
     pub position: V2,
     pub velocity: V2,
@@ -17,6 +37,12 @@ pub struct Particle {
 impl Particle {
     pub fn new(position: V2, velocity: V2) -> Particle {
         Particle { position, velocity }
+    }
+
+    pub fn smoothing_gradient(&self, point: &V2) -> V2 {
+        let d = self.position.sub(point).len();
+        let g = smoothing_kernel_gradient(d);
+        self.position.sub(point).scalar_mul(g)
     }
 }
 
@@ -27,13 +53,32 @@ pub struct World {
     step: f64,
 }
 
+const PARTICLE_MASS: f64 = 1.;
+const PARTICLE_RADIUS: f64 = 4.;
+const KERNEL_INTEGRAL: f64 = 0.36;
+
+// fn kernel_integral() -> f64 {
+//     let r = PARTICLE_RADIUS;
+//     return PI * 2. * (r.powi(2) / 2. - r.powi(3) + 3. * r.powi(4) / 4. - r.powi(5) / 5.);
+// }
+
+fn smoothing_kernel(d: f64) -> f64 {
+    let v = (PARTICLE_RADIUS - d).max(0.) / PARTICLE_RADIUS;
+    v.powi(3) / KERNEL_INTEGRAL
+}
+
+fn smoothing_kernel_gradient(d: f64) -> f64 {
+    let v = (PARTICLE_RADIUS - d).max(0.);
+    -3. * v.powi(2) / KERNEL_INTEGRAL
+}
+
 impl World {
     pub fn new(dimensions: V2, gravity: V2) -> World {
         World {
             particles: Vec::new(),
             dimensions,
             gravity,
-            step: 0.05,
+            step: 0.01,
         }
     }
 
@@ -48,37 +93,78 @@ impl World {
         }
     }
 
+    fn calc_density(&self, particle: &Particle) -> f64 {
+        let mut influence = 0.;
+        for other in &self.particles {
+            let d = particle.position.sub(&other.position).len();
+            influence += smoothing_kernel(d);
+        }
+        influence * PARTICLE_MASS
+    }
+
+    pub fn calc_gradient(&self, point: &V2) -> V2 {
+        let mut gradient = V2::new(0., 0.);
+        for other in &self.particles {
+            let d = point.sub(&other.position).len();
+            if d <= 0.01 {
+                continue;
+            }
+            let g = smoothing_kernel_gradient(d);
+            gradient.x += g * (point.x - other.position.x);
+            gradient.y += g * (point.y - other.position.y);
+        }
+        gradient.scalar_mul(PARTICLE_MASS)
+    }
+
+    pub fn calc_particle_force(&self, particle: &Particle) -> V2 {
+        // let density = self.calc_density(particle);
+        let density = 1.;
+        let pressure = 10.;
+        let gradient = self.calc_gradient(&particle.position);
+        // if gradient.len() > 0. {
+        //     println!("gradient: {:?}", gradient);
+        // }
+        gradient.scalar_mul(-pressure / density)
+    }
+
     pub fn add_particle(&mut self, particle: Particle) {
         self.particles.push(particle);
     }
 
     pub fn evolve(&mut self) {
         let dt = self.step;
-        for particle in &mut self.particles {
-            particle.position.x += particle.velocity.x * dt;
-            particle.position.y += particle.velocity.y * dt;
-            particle.velocity.x += self.gravity.x * dt;
-            particle.velocity.y += self.gravity.y * dt;
+        let friction = 0.99;
+        self.particles = self
+            .particles
+            .iter()
+            .map(|p| {
+                let mut particle = p.clone();
+                let force = self.calc_particle_force(&particle).add(&self.gravity);
+                particle.velocity = particle.velocity.add(&force.scalar_mul(dt));
+                particle.velocity = particle.velocity.scalar_mul(friction);
+                particle.position = particle.position.add(&particle.velocity.scalar_mul(dt));
 
-            if particle.position.x < 0. {
-                particle.position.x = 0.;
-                particle.velocity.x = -particle.velocity.x;
-            }
+                if particle.position.x < 0. {
+                    particle.position.x = 0.;
+                    particle.velocity.x = -particle.velocity.x;
+                }
 
-            if particle.position.x > self.dimensions.x {
-                particle.position.x = self.dimensions.x;
-                particle.velocity.x = -particle.velocity.x;
-            }
+                if particle.position.x > self.dimensions.x {
+                    particle.position.x = self.dimensions.x;
+                    particle.velocity.x = -particle.velocity.x;
+                }
 
-            if particle.position.y < 0. {
-                particle.position.y = 0.;
-                particle.velocity.y = -particle.velocity.y;
-            }
+                if particle.position.y < 0. {
+                    particle.position.y = 0.;
+                    particle.velocity.y = -particle.velocity.y;
+                }
 
-            if particle.position.y > self.dimensions.y {
-                particle.position.y = self.dimensions.y;
-                particle.velocity.y = -particle.velocity.y;
-            }
-        }
+                if particle.position.y > self.dimensions.y {
+                    particle.position.y = self.dimensions.y;
+                    particle.velocity.y = -particle.velocity.y;
+                }
+                return particle;
+            })
+            .collect()
     }
 }
