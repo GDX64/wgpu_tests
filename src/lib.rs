124310625fa0df7d165cbeac40545d5c4d63901e
gdx <gadu.macahdo@gmail.com>
2024-02-11 23:59:46 -0300

    drawing a square

--- src/lib.rs
+++ src/lib.rs
@@ -40,28 +40,24 @@ impl Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
-        tex_coords: [0.4131759, 0.99240386],
+        position: [-0.5, -0.5, 0.0],
+        tex_coords: [0.0, 1.0],
     }, // A
     Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
-        tex_coords: [0.0048659444, 0.56958647],
+        position: [-0.5, 0.5, 0.0],
+        tex_coords: [0.0, 0.0],
     }, // B
     Vertex {
-        position: [-0.21918549, -0.44939706, 0.0],
-        tex_coords: [0.28081453, 0.05060294],
+        position: [0.5, 0.5, 0.0],
+        tex_coords: [1.0, 0.0],
     }, // C
     Vertex {
-        position: [0.35966998, -0.3473291, 0.0],
-        tex_coords: [0.85967, 0.1526709],
+        position: [0.5, -0.5, 0.0],
+        tex_coords: [1.0, 1.0],
     }, // D
-    Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
-        tex_coords: [0.9414737, 0.7347359],
-    }, // E
 ];
 
-const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
+const INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
 
 struct State {
     surface: wgpu::Surface,
@@ -162,7 +158,7 @@ impl State {
                 zfar: 100.0,
                 camera_buffer: None,
             };
-            let mut camera_controller = CameraController::new(1.0, camera);
+            let mut camera_controller = CameraController::new(0.1, camera);
 
             let camera_bind_group_layout =
                 device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
@@ -238,7 +234,7 @@ impl State {
                 topology: wgpu::PrimitiveTopology::TriangleList,
                 strip_index_format: None,
                 front_face: wgpu::FrontFace::Ccw,
-                cull_mode: Some(wgpu::Face::Back),
+                cull_mode: None,
                 // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
                 // or Features::POLYGON_MODE_POINT
                 polygon_mode: wgpu::PolygonMode::Fill,
@@ -331,13 +327,12 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
 
-            //groups
             render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
             render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
-
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+
             render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
